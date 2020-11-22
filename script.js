1269c34b91301c34e70b2ab8292b465b909e3aae
Dipanshu <dipanshubhola1009@gmail.com>
2020-11-23 03:22:26 +0530

    second

---
+++ script.js
@@ -0,0 +1,50 @@
+var dark = false;
+function darkmode(){
+   dark = !dark;
+ 
+   if(dark){
+   document.getElementById("bulb").src = "https://img.icons8.com/ultraviolet/50/000000/reflector-bulb.png" ;
+   document.getElementById("body").style.backgroundColor = "black";
+   document.getElementById("body").style.color = "#ffebcd";
+   for(var i = 0 ; i < document.getElementsByTagName("button").length ; i++){
+     document.getElementsByTagName("button").item(i).style.color = "#ffebcd";
+     document.getElementById("MenuDark").style.backgroundColor="#ffebcd";
+   }
+   for(var i = 0 ; i < document.getElementsByTagName("a").length ; i++){
+     document.getElementsByTagName("a").item(i).style.color = "#ffebcd";
+   }
+   
+   }
+   else{
+   document.getElementById("bulb").src = "https://img.icons8.com/office/50/000000/reflector-bulb.png" ;
+   document.getElementById("body").style.backgroundColor = "white";
+   document.getElementById("body").style.color = "black";
+   for(var i = 0 ; i < document.getElementsByTagName("button").length ; i++){
+     document.getElementsByTagName("button").item(i).style.color = "black";
+   }
+   for(var i = 0 ; i < document.getElementsByTagName("a").length ; i++){
+     document.getElementsByTagName("a").item(i).style.color = "black";
+   }
+   }
+
+}
+
+
+var grid = false;
+
+function viewmode(){
+     grid = !grid;
+     if(grid){
+       document.getElementById("project-grid").style.display = "block";
+       document.getElementById("project-slide").style.display="none";
+       document.getElementById("toggle-view").innerText = "Slide View";
+     }   
+     else{
+       document.getElementById("project-grid").style.display = "none";
+       document.getElementById("project-slide").style.display="flex";
+       document.getElementById("toggle-view").innerText = "Grid View";
+     }
+}
+
+
+
